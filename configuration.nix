edcd152928f46587dec849d7f2d7f41fc97fbb56
kerdamon <kwalas314@gmail.com>
2024-03-25 19:31:05 +0100

    Add steam

--- configuration.nix
+++ configuration.nix
@@ -103,6 +103,12 @@
   #  wget
   ];
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
