08d27b6aae1e9aae5b514cd42b755b3f7a09634a
kerdamon <kwalas314@gmail.com>
2024-09-24 14:04:40 +0200

    Add zsh

--- home.nix
+++ home.nix
@@ -3,24 +3,52 @@
 {
   home.username = "kered";
   home.homeDirectory = "/home/kered";
-
-  nixpkgs.config.allowUnfree = true;
-
   home.stateVersion = "24.05";
+
+  programs.home-manager.enable = true;
+  nixpkgs.config.allowUnfree = true;
 
   home.packages = with pkgs; [
     devbox
+    thefuck
   ];
 
   home.file = {
   };
 
   home.sessionVariables = {
-    ANDROID_HOME = "/home/kered/Android/Sdk";
+    "NIXOS_CONF_PATH" = "/home/kered/.config/home-manager";
   };
 
-  programs.home-manager.enable = true;
+  home.shellAliases = { # aliases for all shells
+    "cdnix" = "cd $NIXOS_CONF_PATH";
+  };
+
+  # programs
 
   programs.vim.enable = true;
-  programs.git.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "kerdamon";
+    userEmail = "kwalas314@gmail.com";
+  };
+  
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" "thefuck" ];
+      theme = "robbyrussell";
+    };
+  };
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
 }
+
