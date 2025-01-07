c9e3eff997452f5b1873462aba872a04129773ab
kerdamon <kwalas314@gmail.com>
2025-01-07 14:01:32 +0100

    [generic-linux][zsh] Setup fzf-tab completions

--- hosts/generic-linux/home.nix
+++ hosts/generic-linux/home.nix
@@ -69,6 +69,7 @@
     enable = true;
     userName = "kerdamon";
     userEmail = "kwalas314@gmail.com";
+    delta.enable = true; # program for nice diff visualisation. Also used in fzf-tab previews.
   };
   
   programs.zsh = {
@@ -77,15 +78,27 @@
     syntaxHighlighting.enable = true;
     defaultKeymap = "viins"; # vi mode
     initExtra = ''
-      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}' # case insensitive matching
+      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'                          # case insensitive matching
+      zstyle ':completion:*' menu no                                                  # don't show completion menu (since I am using fzf-tab plugin)
+      zstyle ':completion:*:git-checkout:*' sort false                                # don't mess with order of git checkout targets
+      zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'   # preview for cd command
+      zstyle ':fzf-tab:*' use-fzf-default-opts yes                                    # To make fzf-tab follow FZF_DEFAULT_OPTS
+      zstyle ':fzf-tab:*' switch-group '<' '>'                                        # switch group using `<` and `>`
+      zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup                                  # use tmux popup
+
+      # nice previews for git. Requires delta.
+      zstyle ':fzf-tab:complete:git-(add|diff|restore):*' fzf-preview 'git diff $word | delta'
+      zstyle ':fzf-tab:complete:git-checkout:*' fzf-preview 'case "$group" in "modified file") git diff $word | delta ;; "recent commit object name") git show --color=always $word | delta ;; *) git log --color=always $word ;; esac'
     '';
     antidote = { # plugin manager
       enable = true;
       plugins = [
         "zsh-users/zsh-completions"
-	"getantidote/use-omz" # oh-my-zsh dependencies - enables to use ozm plugins
+        "getantidote/use-omz" # oh-my-zsh dependencies - enables to use ozm plugins
         # "ohmyzsh/ohmyzsh path:lib" # omz core functionalities (if I understand correctly)
-	"ohmyzsh/ohmyzsh path:plugins/git"
+        "ohmyzsh/ohmyzsh path:plugins/git"
+        "ohmyzsh/ohmyzsh path:plugins/command-not-found" #TODO doesn't work, check how to fix this
+        "Aloxaf/fzf-tab"
       ];
     };
   };
