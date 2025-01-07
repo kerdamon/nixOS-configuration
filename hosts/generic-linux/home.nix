702d9bf0c9b61a02b360d282fc26ced7a1072ff4
kerdamon <kwalas314@gmail.com>
2025-01-07 11:14:03 +0100

    [generic-linux] Add cdt alias

--- hosts/generic-linux/home.nix
+++ hosts/generic-linux/home.nix
@@ -44,6 +44,7 @@
   };
 
   home.shellAliases = { # aliases for all shells
+    "cdt" = "cd ~/tmp";
     "cdnix" = "cd $MY_NIX_CONF_PATH";
     "hms" = "home-manager switch --flake $MY_NIX_CONF_PATH#generic-linux";
     "hm-news" = "home-manager news --flake .#generic-linux";
