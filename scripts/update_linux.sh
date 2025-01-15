14a7462c49f4897a50558d503a8ce277374fac95
kerdamon <kwalas314@gmail.com>
2025-01-15 11:48:39 +0100

    [generic-linux] FIx bugs in update script

--- scripts/update_linux.sh
+++ scripts/update_linux.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 trap "exit" INT # for ctrl + C to exit whole script
 
-echo "> Updating eos (eos-update --nvidia --aur)"
+echo
+echo "--> Updating eos (eos-update --nvidia --aur)"
 echo
 eos-update --nvidia --aur 
 
-echo "> Updating home-manager flake (nix flake update --flake $MY_NIX_CONF_PATH)"
+echo
+echo "--> Updating home-manager flake (nix flake update --flake $MY_NIX_CONF_PATH)"
 echo
 nix flake update --flake $MY_NIX_CONF_PATH
 
-echo "> Applying home-manager configuration (home-manager switch --flake $MY_NIX_CONF_PATH#generic-linux)"
 echo
-hms
+echo "--> Applying home-manager configuration (home-manager switch --flake $MY_NIX_CONF_PATH#generic-linux)"
+echo
+home-manager switch --flake $MY_NIX_CONF_PATH#generic-linux
 
