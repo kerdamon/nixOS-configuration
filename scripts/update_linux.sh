2940b03eecbc8077e0735144e82608da04c95c5c
kerdamon <kwalas314@gmail.com>
2025-01-16 19:06:07 +0100

    Add colors to update_linux script

--- scripts/update_linux.sh
+++ scripts/update_linux.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 trap "exit" INT # for ctrl + C to exit whole script
 
+highlight_color="\033[48;5;53m"
+neutral_color="\033[0m"
+
 echo
-echo "--> Updating eos (eos-update --nvidia --aur)"
+echo "${highlight_color}--> Updating eos$neutral_color (eos-update --nvidia --aur)"
 echo
 eos-update --nvidia --aur 
 
 echo
-echo "--> Updating home-manager flake (nix flake update --flake $MY_NIX_CONF_PATH)"
+echo "${highlight_color}Updating home-manager flake$neutral_color (nix flake update --flake $MY_NIX_CONF_PATH)"
 echo
 nix flake update --flake $MY_NIX_CONF_PATH
 
 echo
-echo "--> Applying home-manager configuration (home-manager switch --flake $MY_NIX_CONF_PATH#generic-linux)"
+echo "${highlight_color}Applying home-manager configuration$neutral_color (home-manager switch --flake $MY_NIX_CONF_PATH#generic-linux)"
 echo
 home-manager switch --flake $MY_NIX_CONF_PATH#generic-linux
 
