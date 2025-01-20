b4f00c0842bb1dd9f689837123ec472bcef52ffd
kerdamon <kwalas314@gmail.com>
2025-01-20 16:12:57 +0100

    [generic-linux] Fix update script

--- scripts/update_linux.sh
+++ scripts/update_linux.sh
@@ -4,18 +4,12 @@ trap "exit" INT # for ctrl + C to exit whole script
 highlight_color="\033[48;5;53m"
 neutral_color="\033[0m"
 
-echo
-echo "${highlight_color}--> Updating eos$neutral_color (eos-update --nvidia --aur)"
-echo
+echo -e "\n${highlight_color}Updating eos$neutral_color (eos-update --nvidia --aur)\n"
 eos-update --nvidia --aur 
 
-echo
-echo "${highlight_color}Updating home-manager flake$neutral_color (nix flake update --flake $MY_NIX_CONF_PATH)"
-echo
+echo -e "\n${highlight_color}Updating home-manager flake$neutral_color (nix flake update --flake $MY_NIX_CONF_PATH)\n"
 nix flake update --flake $MY_NIX_CONF_PATH
 
-echo
-echo "${highlight_color}Applying home-manager configuration$neutral_color (home-manager switch --flake $MY_NIX_CONF_PATH#generic-linux)"
-echo
+echo -e "\n${highlight_color}Applying home-manager configuration$neutral_color (home-manager switch --flake $MY_NIX_CONF_PATH#generic-linux)\n"
 home-manager switch --flake $MY_NIX_CONF_PATH#generic-linux
 
