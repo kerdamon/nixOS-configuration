f6fc5d9d3665882941a9d70505f5e0b0b5fb7ddc
kerdamon <kwalas314@gmail.com>
2025-12-20 22:07:10 +0100

    fix(khl-storage): fix nix conf path

--- hosts/khl-storage/home.nix
+++ hosts/khl-storage/home.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   imports = [
     ../../modules/home-manager/presets/base.nix
@@ -19,11 +19,11 @@
 
   # log off and log in after switching to apply changes
   home.sessionVariables = {
-    "MY_NIX_CONF_PATH" = "/home/kered/Data/nix";
+    "MY_NIX_CONF_PATH" = "${config.home.homeDirectory}/Config/nix"; # TODO put that to base
   };
 
   home.shellAliases = {
-    "nix-switch" = "home-manager switch --flake $MY_NIX_CONF_PATH#generic-linux";
-    "hm-news" = "home-manager news --flake $MY_NIX_CONF_PATH#generic-linux";
+    "nix-switch" = "home-manager switch --flake $MY_NIX_CONF_PATH#khl-storage"; # TODO  get current host from variable and put that to base
+    "hm-news" = "home-manager news --flake $MY_NIX_CONF_PATH#khl-storage";
   };
 }
