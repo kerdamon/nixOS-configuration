0927e5c0657ea40801b7aeca0acb5a0ffea30c2f
kerdamon <kwalas314@gmail.com>
2025-12-20 22:20:42 +0100

    fix: enable targets.genericLinux.enable only on x64

--- modules/home-manager/presets/linux.nix
+++ modules/home-manager/presets/linux.nix
@@ -1,8 +1,12 @@
 # This file contains Home Manager configuration specific to Linux systems
 # TODO move generic stuff to base and workstation stuff to workstation and make conditionals for linux specific only
 
-{ ... }:
+{ lib, pkgs, ... }:
 {
   home.stateVersion = "24.05"; # TODO update?
-  targets.genericLinux.enable = true;
+  targets.genericLinux.enable = lib.mkIf (
+    pkgs.stdenv.hostPlatform.isLinux
+    && pkgs.stdenv.hostPlatform.isx86_64
+    && !pkgs.stdenv.hostPlatform.isNixOS
+  ) true; # For some reason does not work on aarch64. TODO investigate further if this would be beneficial to have on raspberry pi as well
 }
