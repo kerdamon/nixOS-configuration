d87f6dc629c52d2b140134b240c2f19568dfa2bf
kerdamon <kwalas314@gmail.com>
2025-12-27 15:07:29 +0100

    wip

--- scripts/api_calls/dokploy_start_compose.sh
+++ scripts/api_calls/dokploy_start_compose.sh
@@ -1,3 +1,5 @@
-echo "${DOKPLOY_URL}/api/compose.start"
-echo "x-api-key: $(cat ${DOKPLOY_API_KEY_PATH})"
-echo "{\"composeId\": \"$1\"}"
+curl -X POST "${DOKPLOY_URL}/api/compose.start" \
+  -H "x-api-key: $(cat ${DOKPLOY_API_KEY_PATH})" \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d "{\"composeId\": \"$1\"}"
