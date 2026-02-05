0a4379d24cf536c501ba2fc64533c99d08b3ffc7
kerdamon <kwalas314@gmail.com>
2026-02-05 21:28:09 +0100

    feat(terminal): add ssh-copy-kitty-terminfo util

--- modules/home-manager/shared/terminal.nix
+++ modules/home-manager/shared/terminal.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.kitty = {
     enable = true;
@@ -6,4 +6,18 @@
     # enableGitIntegration = true; # TODO debug this, doesn't work for some reason, maybe programs.git.iniContent used by this option doesn't exist
     extraConfig = (builtins.readFile ../../../dotfiles/kitty.conf);
   };
+
+  # Util for copying kitty terminfo from local to server over ssh. Without this using kitty term would glitch.
+  # Needed to be done before first connection to a new server or after kitty breaking update.
+  # https://sw.kovidgoyal.net/kitty/kittens/ssh/#copying-terminfo-files-manually
+  home.packages = [
+    (pkgs.writeShellScriptBin "ssh-copy-kitty-terminfo" ''
+      if [ -z "$1" ]; then
+        echo "Usage: ssh-copy-terminfo <hostname>"
+        exit 1
+      fi
+      infocmp -a xterm-kitty | ssh "$1" tic -x -o \~/.terminfo /dev/stdin
+    '')
+  ];
+
 }
